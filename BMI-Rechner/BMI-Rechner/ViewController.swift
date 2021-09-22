5ce70f3ec595758e7596195fe12edc9899688573
Daniel Boros / BBS2H20A <Daniel.Boros@edu.bib.de>
2021-09-22 12:08:23 +0200

    Layout erstellt (Start- und Hauptmenü) Funktionen --> PickerView für Geschlechterauswahl

---
+++ BMI-Rechner/BMI-Rechner/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  BMI-Rechner
+//
+//  Created by Daniel Boros / BBS2H20A on 22.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let geschlecht = ["männlich", "weiblich"]
+    
+    @IBOutlet weak var geschlechtauswaehlen: UIPickerView!
+        
+    
+    
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        geschlechtauswaehlen.delegate = self
+        geschlechtauswaehlen.dataSource = self
+    
+    }
+
+
+}
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return geschlecht.count
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return geschlecht[row]
+     }
+   }
+    
+

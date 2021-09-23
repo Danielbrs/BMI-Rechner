0c67ad52d5bf6f9ff3c8c35180cff01ae4c7a9a5
Daniel Boros / BBS2H20A <Daniel.Boros@edu.bib.de>
2021-09-23 13:23:11 +0200

    if Anweisungen für die Abschätzung des Gewichts Zurück Button angelegt (um auf die Berechnungsseite zurück zukehren)

--- BMI-Rechner/BMI-Rechner/ViewController.swift
+++ BMI-Rechner/BMI-Rechner/ViewController.swift
@@ -16,41 +16,78 @@ class ViewController: UIViewController {
     @IBOutlet weak var groesseTextField: UITextField!
     
     
-    
-    
     // Variablen für Textfields
     var gewicht = 0.0
     var groesse = 0.0
     var bmi = 0.0
     var ausgabe = 0.0
     var bmikomma = ""
+    var ab = ""
     
-    
+    let vc2 = thirdView(nibName: "thirdView", bundle: nil)
     
     // Ausrechnen des BMI Wertes
     @IBAction func bmiBerechnen(_ sender: UIButton) {
-        
-        let vc2 = thirdView(nibName: "thirdView", bundle: nil)
         
         gewicht = Double(gewichtTextField.text!) ?? 0.0
         groesse = Double(groesseTextField.text!) ?? 0.0
         
         bmi = gewicht / (groesse * groesse)
         bmikomma = String(format: "%.2f", bmi)
-        print(bmikomma)
+        print(bmi)
     
         vc2.texte = bmikomma
         navigationController?.pushViewController(vc2, animated: true)
+        //gewichtseinschaetzung(ab: vc2.abschaetzung)
         
+        if bmi < 30 {
+                
+            ab = "Normalgewicht"
+            print(ab)
+        }
         
+        if bmi < 39.1 && bmi >= 30 {
+                
+            ab = "Übergewicht"
+            print(ab)
+        }
         
+        if bmi > 39.1 {
+                
+            ab = "starkes Übergewicht"
+            print(ab)
+            
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?){
         let destinationVC = segue.destination as! thirdView
         destinationVC.texte = String(bmikomma)
+        destinationVC.abschaetzung = ab
     }
     
+    
+    func gewichtseinschaetzung(ab: String) {
+        
+        if groesse > 1.60 && groesse < 1.70 && gewicht < 75.0 && gewicht > 60 && bmi < 30 {
+                
+            vc2.abschaetzung = "Normalgewicht"
+            
+        }
+        
+        if groesse >= 1.70 && groesse <= 1.80 && gewicht <= 100 && gewicht > 75 && bmi < 39.1 && bmi >= 30 {
+                
+            vc2.abschaetzung = "Übergewicht"
+        }
+        
+        if groesse > 1.80 && groesse <= 1.90 && gewicht >= 100 && bmi > 39.1 {
+                
+            vc2.abschaetzung = " starkes Übergewicht"
+            
+        }
+    }
+
+             
     
     func ausgeben(wert: Double){
         
@@ -77,7 +114,11 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return geschlecht[row]
      }
+    
 }
+
+    
+   
    
 
     
--- BMI-Rechner/thirdView.swift
+++ BMI-Rechner/thirdView.swift
@@ -10,14 +10,23 @@ import UIKit
 class thirdView: UIViewController {
 
     var texte = ""
+    var abschaetzung = ""
     
     @IBOutlet weak var ausgabeLabel: UILabel!
+    @IBOutlet weak var abschaetzungLabel: UILabel!
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         ausgabeLabel.text = texte
+        abschaetzungLabel.text = abschaetzung
     }
+    
+    
     
     
 }
